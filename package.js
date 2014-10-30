4d1dd56e54e906203a0556dab0fd6ccc616f2095
Santiago Gonzalez Toral <hernangt12re3@gmail.com>
2014-10-29 20:50:30 -0500

    Modifications to enable the use of jointjs v0.9.2 on meteor v0.9.3+

--- package.js
+++ package.js
@@ -1,9 +1,16 @@
 Package.describe({
-  summary: "Library for visualization and interaction with diagrams and graphs."
+  summary: "Library for visualization and interaction with diagrams and graphs.",
+  version: "0.9.2",
+  name: "santteegt:jointjs",
+  git: "https://github.com/santteegt/meteor-joint"
 });
 
-Package.on_use(function (api, where) {
-  api.use('jquery', 'client');
-  api.use('underscore', 'client');
+Package.onUse(function (api) {
+  api.versionsFrom('0.9.3.1');
+  api.use('backbone', 'client');
+  api.use('stevezhu:lodash@1.0.2', 'client');
   api.add_files(['joint.js', 'joint.css'], 'client');
+});
+
+Package.onTest(function (api) {
 });
